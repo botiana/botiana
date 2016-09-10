5d3013205ebc1e21224be5c28c309ed88432540e
Ryan Kish <rpkish@gmail.com>
2016-09-09 22:35:34 -0600

    setting up settings to make it more heroku friendly.

---
+++ settings.py
@@ -0,0 +1,14 @@
+# slack token
+token = "<%= ENV['token'] %>"
+
+BOT_NAME = 'botiana'
+
+# max string length for translation
+MAX_TRANSLATE_LENGTH = 250
+
+# http image links for avatar
+icon_ru = "<%= ENV['icon_ru'] %>"
+icon_poolball = "<%= ENV['icon_poolball'] %>"
+icon_tux = "<%= ENV['icon_tux'] %>"
+icon_urban_dictionary = "<%= ENV['icon_urban_dictionary'] %>"
+icon_gw = "<%= ENV['icon_gw'] %>"
